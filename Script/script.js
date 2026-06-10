e91edbec488749fa266570a737b4a7e478acb905
Gemshot <emiliaksiazak@gmail.com>
2026-06-10 08:49:01 +0200

    Emilias Keyboard!
    Not complete but good enough

--- Script/script.js
+++ Script/script.js
@@ -1,6 +1,13 @@
-Document.addEventListener("KeyDown", (event) => {
+document.addEventListener("keydown", (event) => {
     console.log(event);
 
-    document.getElementById(event.code).style = "Background-color: white";
+    document.getElementById(event.code).style = "Background-color: rgb(71, 70, 69);";
 
-});
+});     
+
+document.addEventListener("keyup", (event) => {
+    console.log(event);
+
+    document.getElementById(event.code).style = "Background-color: rgb(181, 169, 156)";
+
+});     
